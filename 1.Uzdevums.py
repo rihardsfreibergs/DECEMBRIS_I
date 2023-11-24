fb8404831555d826ca8d9a3967f102922847a8ef
Rihards <you@example.com>
2023-11-24 09:08:47 +0200

    1 uzd

--- 1.Uzdevums.py
+++ 1.Uzdevums.py
@@ -1,12 +1,12 @@
-#  ievadu skaitli
+#  ievadu skaitli ar inputu
 lietotaja_ievade = int(input("Ievadiet skaitli: "))
 
 # summa
 summa = 0
 
 # for cikls, kas skaita no 1 lidz ievadītajam skaitlim
-for skaitlis in range(1, lietotaja_ievade + 1):
-    summa += skaitlis
+for skaitlis in range(1, lietotaja_ievade ):
+    summa = skaitlis
 
 # rezultāts
 print(f"Summa no 1 līdz {lietotaja_ievade} ir: {summa}")
